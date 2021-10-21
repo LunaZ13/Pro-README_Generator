7e7d3ff8c124cdd03c50070caeff96e8a07ebb84
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2021-10-20 20:26:46 -0500

    questions for user

--- index.js
+++ index.js
@@ -3,10 +3,142 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown =  require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your Project name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter enter a brief description of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the required steps to install your project? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter the required steps to install your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter information on how to use this project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license for your project (Required)',
+        choices: ['MIT', 'Apache 2.0', 'ISC', 'Mozilla', 'GNU GPL v2'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('You must select a license for your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContribution',
+        message: 'Would you like people to contribute to this project?',
+        default: true
+       
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How would you like people to contribute to this project?',
+        when: ({ confirmContribution }) => {
+            if (confirmContribution) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test this project? (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please explain how to test this project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'please provide your email address. (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your your email address!');
+                return false;
+            }
+        }
+    },
+];
+
+
+// both of these inside the .then() of your inqueire!
+// var fakestr = generateMarkdown(answers)
+// writeToFile('readme.md', fakestr)
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // fs.writeFIle stuff here!!
+}
 
 // TODO: Create a function to initialize app
 function init() {}
